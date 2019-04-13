ebf721c87dae71beac99dfc33029c105c440cc5a
Leianne Louis <leiannelouis@Leiannes-MacBook-Pro.local>
2019-04-13 18:00:56 -0400

    added dependencies and created server

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const server = require('./api/server');
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => console.log("Hey I\'m Working!"))
